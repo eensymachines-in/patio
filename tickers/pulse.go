f929f73c0716344cd8de3c9b991489ff159325ce
Niranjan Awati <kneerunjun@gmail.com>
2024-03-06 12:46:23 +0530

    #10 offsetted day calculation bug

--- tickers/pulse.go
+++ tickers/pulse.go
@@ -52,7 +52,7 @@ func PulseEveryDayAt(clock string, pulse time.Duration, ctx context.Context, wg 
 		// time calculations have to be done only inside the go routine since scheduling time of this routine is indeterminate
 		// only when the coroutine gets scheduled can you do all the time calculations.
 		// offDuration, offset := calc_tickOffset(hr, min)
-		_, offset, err := tickTimeUnix(clock)
+		tt, offset, err := tickTimeUnix(clock)
 		if err != nil {
 			log.Error(err)
 			return
@@ -60,6 +60,13 @@ func PulseEveryDayAt(clock string, pulse time.Duration, ctx context.Context, wg 
 		offDuration, _ := time.ParseDuration(fmt.Sprintf("%ds", offset))
 		if offset >= 0 {
 			// case where time until tick duration, so sleeping
+			/*
+				|--- now < you are here
+				------- offset (sleep)
+				|--- tick
+				--- pulse duration(sleep)
+				|--- pulse
+			*/
 			log.WithFields(log.Fields{"offset": offDuration}).Debug("Time until tick")
 			select {
 			case <-time.After(offDuration):
@@ -82,6 +89,12 @@ func PulseEveryDayAt(clock string, pulse time.Duration, ctx context.Context, wg 
 			start := time.Now().Add(offDuration) // offDuration is negative, hence it would give the pulse start
 			end := start.Add(pulse)
 			if time.Until(end) > 0 {
+				/*
+					------ tick
+					|- Sleep (pulse duration)
+					------ now < you are here (send the tick that was missed)
+					------- pulse
+				*/
 				ticks <- time.Now()
 				log.Debug("we are between the pulses")
 				select {
@@ -91,19 +104,31 @@ func PulseEveryDayAt(clock string, pulse time.Duration, ctx context.Context, wg 
 					return
 				}
 			} else {
+				/*
+					------ tick
+					|- Sleep(pulse duration)
+					------ pulse
+					------- now < you are here (no ticks are sent, since the entire pulse is missed)
+				*/
 				log.WithFields(log.Fields{
 					"elapsed": time.Until(end),
 				}).Info("We are beyond ticking time & pulse duration")
 			}
 			// Since we are already pass the pulsing time, the next one pulse shall start for less than 24 hours
 			// offset here is negative, hence the final offset calculated would have to be less than 24 hours / 86400 seconds
-			// BUG: from here the time waited until end is not subtracted if we were between pulses
-			// so pulse width more in waiting offsetted day .. which is wrong
-			offset = int64(86400) + offset
-			offsettedDay, _ := time.ParseDuration(fmt.Sprintf("%ds", offset)) // a day is about 86400 seconds
-			log.WithFields(log.Fields{"offset": offsettedDay}).Debug("time until next tick, offset day")
+			/*
+				I --- tick time
+				I |- sleep
+				I --- pulse end
+				I |- now (in either of the cases since now is beyond tick time, the second cycle is offset-shy of 24 hours
+			*/
+			seconds := 86400 - (time.Now().Unix() - tt) // for the second tick
+			dur, _ := time.ParseDuration(fmt.Sprintf("%ds", seconds))
+			log.WithFields(log.Fields{
+				"duration": dur,
+			}).Debug("ofsetted day")
 			select {
-			case <-time.After(offsettedDay):
+			case <-time.After(dur):
 				ticks <- time.Now()
 			case <-ctx.Done():
 				return
