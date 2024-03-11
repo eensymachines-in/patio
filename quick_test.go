1b812a1c068db401e589904c96461c53fc8a0ce7
Niranjan Awati <kneerunjun@gmail.com>
2024-03-11 18:56:29 +0530

    - successfully tried rabbitmq direct exchange receipt of messages

--- quick_test.go
+++ quick_test.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"testing"
-	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/stretchr/testify/assert"
@@ -12,33 +10,40 @@ import (
 func TestAmqpConnection(t *testing.T) {
 	t.Log("Now starting test")
 	conn, err := amqp.Dial("amqp://guest:guest@192.168.1.102:30073/")
+
 	assert.Nil(t, err, "Unexpected error when connecting to amqp server")
+	defer conn.Close()
 
 	ch, err := conn.Channel()
 	assert.Nil(t, err, "Unexpected error when instantiating channel")
 	assert.NotNil(t, ch, "Unexpected nil channel")
 
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		"aquapone.test", // name
+		false,           // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
 	)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	body := "Hello World!"
-	err = ch.PublishWithContext(ctx,
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	assert.Nil(t, err, "Unexpected nil error when publishing with context")
-	defer conn.Close()
+	assert.Nil(t, err, "Failed to declare queue")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	assert.Nil(t, err, "Failed to start consume channel")
+	var forever chan struct{}
+	go func() {
+		t.Log("Now listening on messages")
+		for d := range msgs {
+			t.Logf("Received a message: %s", d.Body)
+		}
+	}()
+	t.Log(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
