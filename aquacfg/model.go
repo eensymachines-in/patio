fe26ff275563ce85b0bd9d6fe1a11bfa2fa8b34f
kneerunjun <kneerunjun@gmail.com>
2024-04-10 12:33:57 +0000

    aquacfg.AppConfig now hass a public defn for Schedule

--- aquacfg/model.go
+++ aquacfg/model.go
@@ -9,15 +9,17 @@ const (
 	PULSE_EVERY_DAYAT
 )
 
+type Schedule struct {
+	Config   ScheduleType `json:"config"`             // ticking algorithm
+	TickAt   string       `json:"tickat"`             // time of the day ticking /pulsing starts at
+	PulseGap int          `json:"pulsegap,omitempty"` // pulse width incase its pulsing
+	Interval int          `json:"interval,omitempty"` // ticking interval incase its ticking
+}
+
 // AppConfig : object model that captures the configuration for the app in a single run
 // configuration is loaded in the memory once in init, and then stays for the life of the appliation
 // Any change in the configuration has to be enforced my restarting the application
 type AppConfig struct {
-	AppName  string `json:"appname"`
-	Schedule struct {
-		Config   ScheduleType `json:"config"`             // ticking algorithm
-		TickAt   string       `json:"tickat"`             // time of the day ticking /pulsing starts at
-		PulseGap int          `json:"pulsegap,omitempty"` // pulse width incase its pulsing
-		Interval int          `json:"interval,omitempty"` // ticking interval incase its ticking
-	} `json:"schedule"`
+	AppName  string   `json:"appname"`
+	Schedule Schedule `json:"schedule"`
 }
