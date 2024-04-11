329da191c0fe7e7820725a9e9fe2df8fc647e49a
kneerunjun <kneerunjun@gmail.com>
2024-04-11 08:25:48 +0000

    validationfunction for the schedule

--- aquacfg/model.go
+++ aquacfg/model.go
@@ -1,6 +1,12 @@
 package aquacfg
 
+import "regexp"
+
 type ScheduleType uint8
+
+const (
+	INTERVAL_MIN = 10 // short intervals can be detrimental to the life of the relays
+)
 
 const (
 	TICK_EVERY ScheduleType = iota // for every interval the relay just ticks
@@ -14,6 +20,31 @@ type Schedule struct {
 	TickAt   string       `json:"tickat"`             // time of the day ticking /pulsing starts at
 	PulseGap int          `json:"pulsegap,omitempty"` // pulse width incase its pulsing
 	Interval int          `json:"interval,omitempty"` // ticking interval incase its ticking
+}
+
+// IsValid : for the given schedule it checks to see if configuration is not conflicting
+// Depending on what is the schedule type certain values may lead to
+func (sched *Schedule) IsValid() bool {
+	if (sched.Config == TICK_EVERY || sched.Config == PULSE_EVERY) && sched.Interval <= INTERVAL_MIN {
+		// interval cannot be so short - short intervals can lead to shortened life of the relays
+		return false
+	}
+	if (sched.Config == PULSE_EVERY || sched.Config == PULSE_EVERY_DAYAT) && sched.PulseGap <= INTERVAL_MIN {
+		// pulse gap cannot be less than a threshold since it would be then detrimental to the relay life
+		return false
+	}
+	if sched.Config == PULSE_EVERY && (sched.Interval <= sched.PulseGap) {
+		// At no time the pulsegap can be greater than interval
+		return false
+	}
+	if sched.Config == TICK_EVERY_DAYAT || sched.Config == PULSE_EVERY_DAYAT {
+		// time has to specifed for 2 particular configuration that are clock driven
+		expr := regexp.MustCompile(`^[0-9]{2}:[0-9]{2}$`)
+		if !expr.MatchString(sched.TickAt) {
+			return false
+		}
+	}
+	return true
 }
 
 // AppConfig : object model that captures the configuration for the app in a single run
